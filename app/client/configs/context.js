cf0c3af6339a192357354f4765d3b7edeecca339
Stepan <stepan.yurtsiv@yandex.ru>
2016-10-17 11:43:20 +0300

    #31 rm node_modules

--- app/client/configs/context.js
+++ app/client/configs/context.js
@@ -4,6 +4,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router-ssr';
 import { Accounts } from 'meteor/accounts-base';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
+
 export default function() {
 	return {
 		Meteor,
